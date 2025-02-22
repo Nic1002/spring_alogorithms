a182b474bcba149f37a599d4fb291e9a02d43096
salexkon <salexkon@gmail.com>
2025-02-22 19:52:44 +0000

    task_02_done_v1.0

--- task_02/src/main.cpp
+++ task_02/src/main.cpp
@@ -1,3 +1,32 @@
 #include <iostream>
+#include "stack.hpp"
+// #include "stack.cpp"
 
-int main() { return 0; }
+int main() {
+    Stack<int> st {};
+    st.push(5);
+    st.push(4);
+    st.push(3);
+    st.push(7);
+    st.push(8);
+    std::cout << st.pop() << " " << st.pop() << " " << st.pop() << " " << st.pop() << " ";
+    st.push(0);
+    std::cout << st.pop();
+
+    MinStack<int> stm{};
+    stm.push(5);
+    stm.push(7);
+    stm.push(3);
+    stm.push(0);
+    stm.push(1);
+    stm.push(-2);
+    std::cout << stm.getmin() << std::endl;
+    std::cout << stm.pop() << " " << stm.getmin() << std::endl;
+    std::cout << stm.pop() << " " << stm.getmin() << std::endl;
+    std::cout << stm.pop() << " " << stm.getmin() << std::endl;
+    std::cout << stm.pop() << " " << stm.getmin() << std::endl;
+    std::cout << stm.pop() << " " << stm.getmin() << std::endl;
+    std::cout << stm.pop() << " " ;
+    // std::cout << stm.getmin() << stm.getmin() << stm.getmin();
+    return 0; 
+}
--- task_02/src/stack.hpp
+++ task_02/src/stack.hpp
@@ -1,23 +1,50 @@
 #pragma once
 
-#include <stack>
 #include <vector>
+#include <stdexcept>
 
+
+
+template <typename T>
 class Stack {
- public:
-  void Push(int value);
-  int Pop();
+  public:
+  void push(T value) { data_.push_back(value); }
+  T pop() {
+    T result = data_.back();
+    data_.pop_back();
+    return result;
+  }
 
  private:
-  std::stack<int> data_;
+  std::vector<T> data_;
 };
 
+template <typename T>
 class MinStack {
  public:
-  void Push(int value);
-  int Pop();
-  int GetMin();
+  void push(T value) { 
+    data_.push_back(value);
+    if(data_mins.size() == 0 || value <= data_mins.back()){
+      data_mins.push_back(value);
+    } else{
+      data_mins.push_back(data_mins.back());
+    }
+  }
+  T pop() {
+    T result = data_.back();
+    data_.pop_back();
+    data_mins.pop_back();
+    return result;
+  }
+  
+  T getmin() {
+    if(data_mins.size() == 0){
+      throw std::runtime_error("Пустой стек - нет минимума");
+    }
+    return data_mins.back(); 
+  }
 
  private:
-  std::vector<int> data_;
+  std::vector<T> data_;
+  std::vector<T> data_mins;
 };
