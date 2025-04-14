d1535bfeb110bbf16f7f508168b33cb2dababc6a
Haaaydn <kirpichev.ai@phystech.edu>
2025-04-14 15:11:02 +0000

    Добавил другой вариант быстрой сортировки

---
+++ task_05/src/q_sort.hpp
@@ -0,0 +1,11 @@
+/*
+Добавил другой вариант быстрой сортировки
+*/
+
+#include <vector>
+
+int partition(std::vector<int> &arr, int low, int high);
+
+void q_sort(std::vector<int> &arr, int low, int high);
+
+void q_sort_new(std::vector<int> &arr);
