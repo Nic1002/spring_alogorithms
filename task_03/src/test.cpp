0e4c6c402909ca764ed1c78dd92aa80eae99cd7c
Pavel-bur-dot <burachekpavel@gmail.com>
2025-04-26 16:05:47 +0000

    Add tests to task_03

--- task_03/src/test.cpp
+++ task_03/src/test.cpp
@@ -1,8 +1,35 @@
-
 #include <gtest/gtest.h>
+
+#include <vector>
 
 #include "topology_sort.hpp"
 
-TEST(TopologySort, Simple) {
-  ASSERT_EQ(1, 1);  // Stack []
+TEST(TopologyFuncTest, OrdinaryWeek) {
+  std::vector<int> temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
+  std::vector<int> expected = {1, 1, 4, 2, 1, 1, 0, 0};
+  ASSERT_EQ(topology_sort(temperatures), expected);
+}
+
+TEST(TopologyFuncTest, EmptyInput) {
+  std::vector<int> temperatures = {};
+  std::vector<int> expected = {};
+  ASSERT_EQ(topology_sort(temperatures), expected);
+}
+
+TEST(TopologyFuncTest, SingleDay) {
+  std::vector<int> temperatures = {30};
+  std::vector<int> expected = {0};
+  ASSERT_EQ(topology_sort(temperatures), expected);
+}
+
+TEST(TopologyFuncTest, IncreasingTemperatures) {
+  std::vector<int> temperatures = {10, 20, 30, 40};
+  std::vector<int> expected = {1, 1, 1, 0};
+  ASSERT_EQ(topology_sort(temperatures), expected);
+}
+
+TEST(TopologyFuncTest, MixedTemperatures) {
+  std::vector<int> temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
+  std::vector<int> expected = {1, 1, 4, 2, 1, 1, 0, 0};
+  ASSERT_EQ(topology_sort(temperatures), expected);
 }
