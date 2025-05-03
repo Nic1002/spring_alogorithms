e2e5c895fd9f2e30ba6fe84741311b5cffc5b62b
Half-Head <varinitsa.av@phystech.edu>
2025-05-03 08:19:12 +0000

    task05

--- task_05/src/merge.cpp
+++ task_05/src/merge.cpp
@@ -3,33 +3,25 @@
 
 #include "sorting.hpp"
 
-// std::vector<int> United(std::vector<int> first, std::vector<int> second) {
-//   std ::vector<int> ans{};
-//   if ((first.size() == 1) && (second.size() == 1)) {
-//     ans.push_back(std::min(first[0], second[0]));
-//     ans.push_back(std::max(first[0], second[0]));
-//   } else if (first.size() == 1)
-//     return ans;
-// }
+void MergeSort(std::vector<int>& array) {
+  int n = array.size();
+  std::vector<int> buffer(n);
 
-void MergeSort(std ::vector<int>& array) {
-  int i{0}, j{1};
-  int slice{1};
-  while ((j != array.size() - 1) && (i != 0)) {
-    while (i != j) {
-      if (array[i] > array[j]) {
-        iter_swap(array.begin() + i, array.begin() + j);
-        std ::cout << "SWAP";
-        ++j;
-      } else
-        ++i;
-      if ((j - i > slice) || (i == j)) {
-        i = j;
-        j = i + slice;
+  for (int slice = 1; slice < n; slice *= 2) {
+    for (int left = 0; left < n; left += 2 * slice) {
+      int mid = std::min(left + slice, n);
+      int right = std::min(left + 2 * slice, n);
+
+      int i = left, j = mid;
+      int k = left;
+      while (i < mid && j < right) {
+        buffer[k++] = array[i] <= array[j] ? array[i++] : array[j++];
       }
+      while (i < mid) buffer[k++] = array[i++];
+      while (j < right) buffer[k++] = array[j++];
+
+      std::copy(buffer.begin() + left, buffer.begin() + right,
+                array.begin() + left);
     }
-    slice *= 2;
-    slice > array.size() ? j = array.size() - 1 : j = slice;
-    i = 0;
   }
 }
