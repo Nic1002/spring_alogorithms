62eb3b3f4b05092eb2d281f698ccebb15e42fdee
Half-Head <varinitsa.av@phystech.edu>
2025-05-04 13:54:11 +0000

    task09

---
+++ task_09/src/TableTask.hpp
@@ -0,0 +1,6 @@
+#include <string>
+#include <vector>
+
+std::vector<std::string> TaskSolve(int n, int m, int k,
+                                   std::vector<std::vector<int>>& table,
+                                   std::vector<std::pair<int, int>>& queries);
