7db5c52d3d004f93d47932fb455ae272fde00a15
salexkon <salexkon@gmail.com>
2025-06-21 10:02:43 +0000

    task_01 clang-format

--- task_01/src/task_01.cpp
+++ task_01/src/task_01.cpp
@@ -1,18 +1,18 @@
 #include "task_01.hpp"
 
-std::vector<int> find_two_nums(int target, std::vector<int> arr){
-    int left = 0;
-    int right = arr.size() - 1;
-    int sum = arr[right] + arr[left];
-    while (left < right) {
-        int sum = arr[left] + arr[right];
-        if(sum == target){
-            return {arr[left], arr[right]};
-        } else if(sum < target){
-            ++left;
-        } else {
-            --right;
-        }
+std::vector<int> find_two_nums(int target, std::vector<int> arr) {
+  int left = 0;
+  int right = arr.size() - 1;
+  int sum = arr[right] + arr[left];
+  while (left < right) {
+    int sum = arr[left] + arr[right];
+    if (sum == target) {
+      return {arr[left], arr[right]};
+    } else if (sum < target) {
+      ++left;
+    } else {
+      --right;
     }
-    return {};
+  }
+  return {};
 };
